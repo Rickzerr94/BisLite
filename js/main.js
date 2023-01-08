e0dcdd83eaa2dd5efadcfa2a07d212205d2e69f4
Rickzerr94 <rickzerr91@gmail.com>
2023-01-08 19:01:32 +0200

    4th-section slider in process +add arrows

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ $(function(){
         dots: true,
     });
     $('.works__inner').slick({
-        arrows: false,
+        // arrows: false,
         slidesToShow: 4,
         slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slide-left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/slide-right.svg" alt="right"></button>',
     });
 
 });
