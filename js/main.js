d21a477fceb392e87c2000d930bcaa47afb42738
Rickzerr94 <rickzerr91@gmail.com>
2023-02-17 15:12:52 +0200

    add adaptive

--- js/main.js
+++ js/main.js
@@ -8,8 +8,25 @@ $(function(){
         // arrows: false,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 860,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+                breakpoint: 428,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            }
+        ],
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/slide-left.svg" alt="left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/slide-right.svg" alt="right"></button>',
+    });
+    $('.menu__btn').on('click', function (){
+       $('.header__menu-list').toggleClass('header__menu-active');
     });
 
 });
